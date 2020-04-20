78abaaf99516835c136fef57562fbf62a0c18b0a
nucklass <nicolas.padillas@gmail.com>
2020-04-20 09:52:05 -0700

    writing iris dataset using sas; loading csv from url

--- sqldatabaseconnect.jl
+++ sqldatabaseconnect.jl
@@ -19,36 +19,58 @@ pass= begin
     readline()
 end 
 
-#get the grid datasets from Cal Poly
-gridusers=download("https://web.calpoly.edu/~rottesen/Stat441/Sasdata/Sasdata/users.sas7bdat") |> readsas |> table
+######################## Data that fits in memory and using Tables.jl data sinks ##################################################
+#get the grid datasets from Cal Poly, convert from sas7bdat to IndexedTable 
+#we'll be using the Indexed table as the datasink as it works well with larger datasets
+#but the choice is arbitray in this case and can be a dataframe, dict or some Tables.jl implementation
 
-gridprojects = download("https://web.calpoly.edu/~rottesen/Stat441/Sasdata/Sasdata/projects.sas7bdat") |> readsas |> table
+gridusers = download("https://web.calpoly.edu/~rottesen/Stat441/Sasdata/Sasdata/users.sas7bdat") |> 
+            readsas |> 
+            table
+
+gridprojects = download("https://web.calpoly.edu/~rottesen/Stat441/Sasdata/Sasdata/projects.sas7bdat") |> 
+                readsas |> 
+                table
 
 # setting up database  
-dsn=ODBC.DSN("Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=$database;UID=$user;PWD=$pass;")
+dsn = ODBC.DSN("Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=$database;UID=$user;PWD=$pass;")
+
+ODBC.execute!(dsn,"drop table if exists users; 
+drop table if exists projects")
 
 #writing projects data
+
 ## create the tables
-ODBC.execute!(dsn,"create table projects (Class VARCHAR(10), id INT, StartDate INT, EndDate INT)")
-ODBC.execute!(dsn,"create table users () ")
+ODBC.execute!(dsn,"create table projects 
+(Class VARCHAR(10), id INT, StartDate INT, EndDate INT)")
+
+ODBC.execute!(dsn,
+"create table users 
+(Class VARCHAR(10), Fname VARCHAR(16), Lname VARCHAR(16), Email VARCHAR(24), Phone VARCHAR(9), Dept VARCHAR(5), id INT) ")
+
 ## make a sql statement with "blank" values that we'll put in actual values into 
 insertstmt=ODBC.prepare(dsn,"insert into projects values(?,?,?,?)")
 
 ## now for each row in the rows of gridprojects, run the previous insert statemet, with the values for each row 
 ## inserted into the previous "blank rows" ie those question marks
 
-
 for row in rows(gridprojects)
     ODBC.execute!(insertstmt,row)
 end
 
+## make a sql statement with "blank" values that we'll put in actual values into 
+insertstmt=ODBC.prepare(dsn,"insert into projects values(?,?,?,?,?,?,?)")
+
+## now for each row in the rows of gridusers, run the previous insert statemet, with the values for each row 
+## inserted into the previous "blank rows" ie those question marks
 for row in rows(gridusers)
     ODBC.execute!(insertstmt,row)
 end
 
 
-
 # select top 50 rows from sample table, select as a julia DB indexed table. 
-results=ODBC.query(dsn,"select TOP 50 * from projects") |> table
+results=ODBC.query(dsn,"select TOP 50 * from projects") |> 
+        table
+
 ODBC.disconnect!(dsn)
 
--- sqldatabaseconnect.sas
+++ sqldatabaseconnect.sas
@@ -1,8 +1,20 @@
+/** This code is for the SAS windowing environment only. **/
+
+/** %WINDOW defines the prompt **/
+%window info
+  #5 @5 'Please enter userid:'
+  #5 @26 id 8 attr=underline
+  #7 @5 'Please enter password:'
+  #7 @28 pass 8 attr=underline display=no;
+
+/** %DISPLAY invokes the prompt **/
+%display info;
+
 /*from proc sql directly*/
 /*this is pretty convoluted*/
 proc sql;
 connect to odbc as conn
-required="Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=ntsb;UID=ntsb;PWD=Cessna182";
+required="Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=NickDb;UID=&id;PWD=&pass";
 
 create table event as
 select * from connection to conn
@@ -11,10 +23,25 @@ select * from connection to conn
 disconnect from conn;
 quit;
 
-/*by using a libname*/
+/*by using a libname, much more straightforward*/
 libname conn2 odbc
-required ="Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=ntsb;UID=ntsb;PWD=Cessna182";
+required ="Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=NickDb;UID=&id;PWD=&pass";
+
+/*read data into work directory*/
 proc sql;
 create table event as
 select * from conn2.events ;
+quit;
+
+/*writing data from work directory to database*/
+filename download url "https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv";
+
+data iris;
+infile download delimiter = "," firstobs=2;
+input var1-var4 species $;
+run;
+
+proc sql;
+create table conn2.iris as 
+(select * from iris);
 quit;
