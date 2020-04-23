7ba61e3c0947c0d1ff92fa268ccde0e0af05bce4
jennalandy <jennalandy0@gmail.com>
2020-04-23 15:05:03 -0700

    transitioning to bookdown

---
+++ bookdown/README.md
@@ -0,0 +1,8 @@
+# Writing Pages
+- Number pages in order. 
+- Page title is defined by `#` top markdown header
+- Subsections defined by lower markdown headers
+
+# Knitting Book
+- In the `bookdown` directory, run `render_book("index.Rmd")`.
+- Knitted book saved as `bookdown/_book/index.html`.
--- sqldatabaseconnect.r
+++ sqldatabaseconnect.r
@@ -1,11 +1,3 @@
-# Old code
-# library(RODBC)
-# # tried the tidyverse ODBC package, but it kept giving me errors
-# library(tidyverse)
-# conn=odbcDriverConnect(connection="Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=ntsb;UID=ntsb;PWD=Cessna182;")
-# dat =conn%>% sqlQuery("select * from events")
-# close(conn)
-
 library(odbc)
 library(DBI)
 library(datasets)
