9285b4ffc2d6375a5a4c53b56f6a1a4080d0fb6e
nucklass <nicolas.padillas@gmail.com>
2020-05-14 09:15:44 -0700

    added api section

--- api.jl
+++ api.jl
@@ -1,10 +1,9 @@
 using HTTP, JSON, DataFrames, Query
 
-urlcurrent = "https://covidtracking.com/api/v1/us/current.json"
-HTTP.get(urlcurrent)
-
 urlcacurrent = "https://covidtracking.com/api/v1/states/CA/current.json"
 
+rawdata = HTTP.get(urlcacurrent)
 
+JSONdata = rawdata.body |> String
 
-
+CACOVID = JSONdata |> JSON.parse |> DataFrame
--- bakery_app/app.R
+++ bakery_app/app.R
@@ -19,6 +19,18 @@
 library(shiny)
 library(DT)
 library(tidyverse)
+library(JuliaCall)
+library(tidyverse)
+julia_install_package_if_needed("ODBC")
+julia_install_package_if_needed("DataFrames")
+julia_library("ODBC")
+julia_library("DataFrames")
+#source helpers
+setwd("/home/datguy/Documents/stat440/bakery_app")
+julia_source("app_utils.jl")
+dsn =julia_eval('
+ ODBC.DSN("Driver={ODBC Driver 17 for SQL Server};Address=24.205.251.117;Database=NickDb;UID=Nick;PWD=WaveTrack3@;")
+')
 
 source('app_utils.R')
 
@@ -56,18 +68,32 @@ ui <- fluidPage(
                     DTOutput("out")
                 )
             )
-        )
+        ),
         
         # Panel 2: Writing to Database
-    )
+        tabPanel(
+             titlePanel("Upload to nick database"),
+             sidebarLayout(
+                 sidebarPanel(
+                    selectInput("table_name", "Select Table to Append To", julia_call("ODBC.query",dsn,"select name from sys.tables"))
+                    ),
+
+                mainPanel(
+                # This outputs the dynamic UI component
+                verbatimTextOutput("status"),
+                uiOutput("ui"),
+                actionButton("upload","Upload Data Row"))
+             )
+           
+             
+        )
+    )    
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
     output$choice_table <- renderPrint({ input$select_table })
-    
-    
     output$select_columns <- renderUI({
         column_info <- get_columns(input$select_table)
         print(column_info)
@@ -94,7 +120,30 @@ server <- function(input, output) {
         output$out <- getDT(input, output)
       }
     )
-        
+    output$ui <- renderUI({
+    # Depending on input$input_type, we'll generate a different
+    # UI component and send it to the client.
+        cols=julia_call("getcols",dsn,input$table_name)
+    
+        lapply(1:length(cols), function(i) {
+            textInput(paste0('a', i),paste("Input Value for Column ",cols[i]))
+        })
+   
+    })
+
+    observeEvent(input$upload, {
+    cols=julia_call("getcols",dsn,input$table_name)
+    y=NULL
+
+    for (i in 1:length(cols)){
+        y[i]= eval(parse(text=paste0('input$a',i)))
+    }
+    y=paste(y,collapse=",")
+      print(y)
+      sta=julia_call("addrow", dsn, input$table_name, y)
+    output$status <- renderText({sta})
+    })
+
 }
 
 shinyApp(ui = ui, server = server)
